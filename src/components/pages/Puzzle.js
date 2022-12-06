fd8688cb8f035009ec3821abdc980de2c5714c14
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-06 15:23:45 -0500

    initial assets complete

---
+++ src/components/pages/Puzzle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import room from './../../assets/room.png'
+
+export default function Puzzle () {
+    return (
+        <div className='puzzle-container'>
+            <p className='puzzle-timer'>Timer</p>
+            <div className='puzzle-image'>
+                <img id='room-img' src='room' alt='an empty room with red walls'/>
+            </div>
+            <div className='puzzle-inventory'>
+                {/* {conditional rendering of items} */}
+            </div>
+        </div>
+    )
+}
