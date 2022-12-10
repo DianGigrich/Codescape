7dbb58c2f7ac161721ccb222348bf6e49fd289eb
Dian Sherman <slayer_barrett_@hotmail.com>
2022-12-10 14:26:09 -0800

    added interactivity to the welcome page

--- src/components/Question.js
+++ src/components/Question.js
@@ -2,97 +2,16 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { Button } from '@mui/material/'
+import { Button, Stack } from '@mui/material/'
 import '../index.css';
-import * as interact from 'interactjs';
-
-function dragMoveListener(event) {
-  var target = event.target
-  // keep the dragged position in the data-x/data-y attributes
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-  // translate the element
-  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-  // update the posiion attributes
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
-}
-
-// this function is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener
-
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: ['#pizza-drop', '#pasta-drop'],
-
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
-
-  // listen for drop related events:
-
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active')
-  },
-
-  ondragenter: function (event) {
-  var draggableElement = event.relatedTarget
-  var dropzoneElement = event.target
-
-  // feedback the possibility of a drop
-  dropzoneElement.classList.add('drop-target')
-  draggableElement.classList.add('can-drop')
-  },
-
-  ondragleave: function (event) {
-    // remove the drop feedback style
-    event.target.classList.remove('drop-target')
-    event.relatedTarget.classList.remove('can-drop')
-  },
-
-  ondrop: function (event) {
-    if (event.target.innerText === 'Pizza' && event.relatedTarget.innerText === "Pizza") {
-      event.relatedTarget.classList.remove('drag-drop')
-      
-      event.relatedTarget.classList.add('drag-stop')
-      
-    } else {
-      event.relatedTarget.classList.remove('drop-active')
-      event.relatedTarget.classList.remove('drop-target')
-      event.relatedTarget.classList.add('drop-wrong')
-    }
-  },
-
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active')
-    event.target.classList.remove('drop-target')
-    event.target.classList.add('drop-wrong')
-  }
-})
-
-interact('.drag-drop')
-  .draggable({
-    inertia: true,
-    modifiers: [
-      interact.modifiers.restrictRect({
-        restriction: 'parent',
-        endOnly: true
-      })
-    ],
-    autoScroll: true,
-    // dragMoveListener from the dragging demo above
-    listeners: { move: dragMoveListener }
-  })
+// import * as interact from 'interactjs';
 
 export default function Question({ open, setOpen }) {
   const handleClose = () => setOpen(false);
 
   return (
     <div>
-      <Modal
+      <Modal id="Modalrestriction"
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
@@ -123,18 +42,25 @@ export default function Question({ open, setOpen }) {
             How many licks does it take to get to the center of a tootsie pop?
           </Typography>
           <Stack
-                        sx={{ pt: 4 }}
-                        direction="row"
-                        spacing={2}
-                        justifyContent="center"
-                    >
-          <div id="inner-dropzone" className="dropzone">Pizza </div>
-          <div id="second-dropzone" className="dropzone">Pasta </div>
-          <div id="pizza-drop" className="drag-drop"> Pizza </div>
-          <div id="pasta-drop" className="drag-drop"> Pasta </div>
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <div id="inner-dropzone" className="dropzone">Pizza </div>
+            <div id="second-dropzone" className="dropzone">Pasta </div>
 
-                    </Stack>
+          </Stack>
 
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <div id="pizza-drop" className="drag-drop"> Pizza </div>
+            <div id="pasta-drop" className="drag-drop"> Pasta </div>
+          </Stack>
           <Button type="submit" variant="contained" color="primary" sx={{ mt: 3, mb: 2 }} onClick={handleClose}>Button</Button>
         </Box>
       </Modal>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
+// import { styled } from '@mui/material/styles';
+// import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { Container, Box, Stack, Button } from '@mui/material/'
 import '../../index.css';
+import * as interact from 'interactjs';
+
 
 
 
@@ -20,6 +22,103 @@ import '../../index.css';
 //     }),
 // }));
 
+
+function dragMoveListener(event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  
+    // translate the element
+    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+  
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
+  }
+  
+  // this function is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener
+  
+  interact('.dropzone').dropzone({
+    // only accept elements matching this CSS selector
+    accept: ['#pizza-drop', '#pasta-drop'],
+  
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
+  
+    // listen for drop related events:
+  
+    ondropactivate: function (event) {
+      // add active dropzone feedback
+      event.target.classList.add('drop-active')
+    },
+  
+    ondragenter: function (event) {
+      var draggableElement = event.relatedTarget
+      var dropzoneElement = event.target
+  
+      // feedback the possibility of a drop
+      dropzoneElement.classList.add('drop-target')
+      draggableElement.classList.add('can-drop')
+    },
+  
+    ondragleave: function (event) {
+      // remove the drop feedback style
+      event.target.classList.remove('drop-target')
+      event.relatedTarget.classList.remove('can-drop')
+    },
+  
+    ondrop: function (event) {
+      if (event.target.innerText === 'Pizza' && event.relatedTarget.innerText === "Pizza") {
+        event.relatedTarget.classList.remove('drag-drop')
+  
+        event.relatedTarget.classList.add('drag-stop')
+  
+      } else {
+        event.relatedTarget.classList.remove('drop-active')
+        event.relatedTarget.classList.remove('drop-target')
+        event.relatedTarget.classList.add('drop-wrong')
+      }
+    },
+  
+    ondropdeactivate: function (event) {
+      // remove active dropzone feedback
+      event.target.classList.remove('drop-active')
+      event.target.classList.remove('drop-target')
+      event.target.classList.add('drop-wrong')
+    }
+  })
+  
+  interact('.drag-drop')
+    .draggable({
+      inertia: true,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'self',
+          endOnly: true
+        })
+      ],
+      autoScroll: true,
+      // dragMoveListener from the dragging demo above
+      listeners: { move: dragMoveListener }
+    })
+
+    interact('.welcome')
+    .draggable({
+      inertia: true,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'parent',
+          endOnly: true
+        })
+      ],
+      autoScroll: true,
+      // dragMoveListener from the dragging demo above
+      listeners: { move: dragMoveListener }
+    })
+
+    
 export default function Home() {
 
     return (
@@ -34,15 +133,61 @@ export default function Home() {
                 }}
             >
                 <Container maxWidth="sm">
+                    <Stack sx={{ pt: 4 }}
+                        direction="row"
+                        spacing={2}
+                        justifyContent="center" >
                     <Typography
                         component="h1"
                         variant="h2"
                         align="center"
                         color="text.primary"
                         gutterBottom
+                        className="welcome"
                     >
-                        const Hi = "Welcome"
+                        const
+                        </Typography>
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                        className="welcome"
+                    >
+                       Hi
                     </Typography>
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                        className="welcome"
+                    >
+                        =
+                    </Typography>
+                   <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                        className="welcome"
+                    >
+                        "Welcome"
+                    </Typography>
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                        className="welcome"
+                    >
+                        ;
+                    </Typography>
+                    </Stack>
                     <br />
                     <Typography variant="h5" align="center" color="text.secondary" paragraph>
                         Play an escape room game to practice your javascript skills!  There are questions from beginner to advanced to help you improve.  Beat the time, escape the room, and practice, practice, practice!
@@ -57,20 +202,6 @@ export default function Home() {
                         <Button variant="outlined" color="secondary">This also does nothing</Button>
                     </Stack>
 
-                </Container>
-                <Container>
-                    <div className="drag-drop" id='drag-1'> MOVE ME</div>
-                    <div id="no-drop" className="drag-drop"> #no-drop </div>
-
-
-                    <div id="second-dropzone" className="dropzone">
-                        #outer-dropzone
-                    </div>
-                    <div id="yes-drop" className="drag-drop"> #yes-drop </div>
-                    <Box id="grid-snap">
-                    <div id="inner-dropzone" className="dropzone">#inner-dropzone</div>
-                        Dragging is constrained to the corners of a grid
-                    </Box>
                 </Container>
             </Box>
 
