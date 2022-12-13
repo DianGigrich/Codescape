dbbd98e5d018a3574e6beff80cada53e10607eb9
killingsworth.kristen <killingsworth_kristen@yahoo.com>
2022-12-13 17:28:37 -0500

    machiko code

--- src/components/pages/RoomOfError.js
+++ src/components/pages/RoomOfError.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style/RoomOfError.css';
 import { Box } from '@mui/material/';
-
+import Timer from './../Timer'
 
 
 function RoomOfError () {
@@ -12,7 +12,9 @@ function RoomOfError () {
         
         <Box className='puzzle-container'>
         <div id='puzzle-timer-container'>
-            <p id='puzzle-timer'>Timer</p>
+            <p id='puzzle-timer'>
+                Time Left: <Timer max={300}/>
+            </p>
         </div>
         <div id='room-of-error-frame-container'>
             <iframe id='room-of-error-frame' title='room-of-error-image' src='/room-of-error-frame' scrolling="no" style={{width: '800px', height: '652px', display: 'flex', justifySelf: 'space-around', overflow: 'hidden'}}/>
